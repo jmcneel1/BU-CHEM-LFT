4e70d29e7aa966d1fc5f3be05b2adcd47d556e8e
jmcneel1 <jamesmcneely@mac.com>
2023-10-16 20:50:39 -0400

    Update bulft_get_parameters.h

--- include/bulft_get_parameters.h
+++ include/bulft_get_parameters.h
@@ -1,11 +1,18 @@
 #ifndef _bulft_get_parameters
 #define _bulft_get_parameters
 
+#include <iostream>
 #include "bulft_sys.h"
 
 namespace BULFT 
 {
-  BULFT::BUlftSys GetParameters();
+  BULFT::BUlftSys GetParameters ()
+  {
+    std::cout << "Welcome to BU-CHEM-LFT!\n\n";
+    std::cout << "This program will generate CSFs and determine the coupling";
+    std::cout << " between the different terms based on the orbital angular momentum operator";
+    std::cout << " and the spin-orbit coupling operator.\n\n";
+  }
 }
 
 #endif
