8db1655df9422d2a187a61e8c9ddd6935d401394
jmcneel1 <jamesmcneely@mac.com>
2025-09-29 20:40:50 -0400

    Update bulft_get_parameters.h

--- include/bulft_get_parameters.h
+++ include/bulft_get_parameters.h
@@ -8,10 +8,17 @@ namespace BULFT
 {
   BULFT::BUlftSys GetParameters ()
   {
+    short nel;
     std::cout << "Welcome to BU-CHEM-LFT!\n\n";
     std::cout << "This program will generate CSFs and determine the coupling";
     std::cout << " between the different terms based on the orbital angular momentum operator";
     std::cout << " and the spin-orbit coupling operator.\n\n";
+    std::cout << "The G-Tensor will derived as well as the ZFS tensor\n\n";
+    std::cout << "Output will be given in LaTex format...\n\n";
+    std::cout << "There are 10 spin-orbitals that are available for occupation:\n";
+    std::cout << "0: xz(alpha)\n1: xz(beta)\nyz(alpha)\nyz(beta)";
+    std::cout << "How many electrons are in your system? ";
+    std::cin >> nel;
   }
 }
 
